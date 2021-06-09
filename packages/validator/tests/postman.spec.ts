9a10cd0d8d563f0765142a0f1a85909cf489d81c
Maksym Hysko <hysko.mv@gmail.com>
2021-06-09 23:04:30 +0300

    feat(validator): postman tests
    closes #6

---
+++ packages/validator/tests/postman.spec.ts
@@ -0,0 +1,68 @@
+import nexplotPostman from './NexPloit_api.postman.json';
+import { DefaultValidator, Validator } from '../src';
+import { Postman } from '@har-sdk/types';
+import { expect } from 'chai';
+
+describe('Postman', async () => {
+  const validator: Validator = new DefaultValidator();
+
+  it('Valid JSON', async () => {
+    const result = await validator.verify(
+      nexplotPostman as unknown as Postman.Document
+    );
+    expect(result.errors.length).to.be.equal(0);
+  });
+
+  it('Invalid JSON: cannot determine version of document', async () => {
+    const apiDoc = {
+      info: {
+        title: 'Some valid API document',
+        version: '1.0.0'
+      },
+      paths: {}
+    };
+
+    try {
+      await validator.verify(apiDoc as unknown as Postman.Document);
+    } catch (err) {
+      expect(err.message).to.be.equal(
+        'Cannot determine version of schema. Schema ID is missed.'
+      );
+    }
+  });
+
+  it('Invalid JSON: invalid document', async () => {
+    const apiDoc = {
+      info: {
+        title: 'Some valid API document',
+        schema: 'https://schema.getpostman.com/json/collection/v2.0.0/#',
+        version: '2.0.0'
+      }
+    };
+    const errors = [
+      {
+        instancePath: '',
+        schemaPath: '#/required',
+        keyword: 'required',
+        params: {
+          missingProperty: 'item'
+        },
+        message: "must have required property 'item'"
+      },
+      {
+        instancePath: '/info',
+        schemaPath: '#/required',
+        keyword: 'required',
+        params: {
+          missingProperty: 'name'
+        },
+        message: "must have required property 'name'"
+      }
+    ];
+
+    const result = await validator.verify(
+      apiDoc as unknown as Postman.Document
+    );
+    expect(result.errors).to.deep.equal(errors);
+  });
+});

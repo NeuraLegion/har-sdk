a36d7b3054c51409ed22f24df5af14da620ed385
Viachaslau Tyshkavets <pmstss@gmail.com>
2021-10-19 12:25:31 +0300

    feat: schema editor
    - codeclimate: split long methods
    
    closes #21

--- packages/editor/src/parser/oas3/ParameterObjectsParser.ts
+++ packages/editor/src/parser/oas3/ParameterObjectsParser.ts
@@ -25,28 +25,29 @@ export class ParameterObjectsParser implements ParametersParser {
       (
         parameter: OpenAPIV3.ReferenceObject | OpenAPIV3.ParameterObject,
         idx: number
-      ): SpecTreeLocationParam => {
-        const paramJsonPointer = `${pointer}/${idx}`;
-        const $ref = (parameter as OpenAPIV3.ReferenceObject).$ref;
-
-        const paramObj = $ref
-          ? jsonPointer.get(this.dereferencedDoc, paramJsonPointer)
-          : (parameter as OpenAPIV3.ParameterObject);
-        const value =
-          paramObj.example ||
-          (paramObj.schema as OpenAPIV3.SchemaObject).default;
-
-        return {
-          paramType: 'location',
-          name: paramObj.name,
-          ...(value != null ? { value } : {}),
-          valueJsonPointer: `${pointer}${jsonPointer.compile([
-            idx.toString(10),
-            'example'
-          ])}`,
-          location: paramObj.in as ParamLocation
-        };
-      }
+      ): SpecTreeLocationParam =>
+        this.parseParameter(`${pointer}/${idx}`, parameter)
     );
+  }
+
+  private parseParameter(
+    pointer: string,
+    parameter: OpenAPIV3.ReferenceObject | OpenAPIV3.ParameterObject
+  ): SpecTreeLocationParam {
+    const $ref = (parameter as OpenAPIV3.ReferenceObject).$ref;
+
+    const paramObj = $ref
+      ? jsonPointer.get(this.dereferencedDoc, pointer)
+      : (parameter as OpenAPIV3.ParameterObject);
+    const value =
+      paramObj.example || (paramObj.schema as OpenAPIV3.SchemaObject).default;
+
+    return {
+      paramType: 'location',
+      name: paramObj.name,
+      ...(value != null ? { value } : {}),
+      valueJsonPointer: `${pointer}/example`,
+      location: paramObj.in as ParamLocation
+    };
   }
 }

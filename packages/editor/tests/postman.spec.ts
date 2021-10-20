3387f74fc93b9ef62a367f1870264fcdd5492200
Viachaslau Tyshkavets <pmstss@gmail.com>
2021-10-20 11:16:35 +0300

    feat: schema editor
    - postman spec: some tests for editor functionality
    - bugfixes
    
    closes #21

--- packages/editor/tests/postman.spec.ts
+++ packages/editor/tests/postman.spec.ts
@@ -1,5 +1,6 @@
-import { TreeParser, PostmanEditor } from '../src';
-
+import { TreeParser, PostmanEditor, SpecTreeNodeParam } from '../src';
+import { Postman } from '@har-sdk/types';
+import jsonPath from 'jsonpath';
 import 'chai/register-should';
 import { PostmanValidator } from '@har-sdk/validator';
 import chaiAsPromised from 'chai-as-promised';
@@ -42,6 +43,69 @@ describe('oas parser', () => {
       await postmanTreeParser.setup(source);
       const tree = postmanTreeParser.parse();
       tree.should.deep.eq(resultTree);
+    });
+  });
+
+  describe('postman editor', () => {
+    const postmanEditor = new PostmanEditor();
+
+    const shouldBeValidDoc = (doc: Postman.Document) =>
+      new PostmanValidator().verify(doc).should.eventually.deep.eq({
+        errors: [],
+        valid: true
+      });
+
+    describe('postman parameters editor', () => {
+      it('should set global variable value', async () => {
+        const newValue = 'https://neuralegion.com';
+
+        await postmanEditor.setup(source);
+        let tree = postmanEditor.parse();
+        tree = postmanEditor.setParameterValue(
+          tree.parameters[0].valueJsonPointer,
+          newValue
+        );
+
+        postmanEditor['doc'].variable[0].value.should.equal(newValue);
+
+        shouldBeValidDoc(postmanEditor['doc']);
+
+        tree.parameters.should.deep.equal([
+          {
+            paramType: 'variable',
+            name: 'baseUrl',
+            valueJsonPointer: '/variable/0/value',
+            value: newValue
+          }
+        ]);
+      });
+
+      it('should change path param value', async () => {
+        const path =
+          '$..children[?(@.path=="{{baseUrl}}/api/v1/subscriptions/:subscriptionId" && @.method=="GET")].parameters[?(@.name=="subscriptionId")]';
+        const newValue = 'id42';
+
+        await postmanEditor.setup(source);
+
+        let tree = postmanEditor.parse();
+        const queryParam = jsonPath.query(tree, path)[0] as SpecTreeNodeParam;
+        queryParam.value.should.eq('<string>');
+
+        tree = postmanEditor.setParameterValue(
+          queryParam.valueJsonPointer,
+          newValue
+        );
+
+        shouldBeValidDoc(postmanEditor['doc']);
+
+        jsonPath.query(tree, path)[0].value.should.equal(newValue);
+
+        const docPath =
+          '$..item[?(@.request.url.raw=="{{baseUrl}}/api/v1/subscriptions/:subscriptionId")].request.url.variable[?(@.key=="subscriptionId")]';
+        jsonPath
+          .query(postmanEditor['doc'], docPath)[0]
+          .value.should.equal(newValue);
+      });
     });
   });
 });

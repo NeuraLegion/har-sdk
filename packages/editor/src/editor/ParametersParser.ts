1fbad0fa19960c3124216806d77f709e4d7096d1
Viachaslau Tyshkavets <pmstss@gmail.com>
2021-11-08 21:01:40 +0300

    fix(editor): pr requested changes
    - code style: typo fix

--- packages/editor/src/editor/ParametersParser.ts
+++ packages/editor/src/editor/ParametersParser.ts
@@ -1,4 +1,4 @@
 import { SpecTreeNodeParam } from '../models';
 import { Parser } from './Parser';
 
-export type ParametersParser = Parser<SpecTreeNodeParam[] | undefiend>;
+export type ParametersParser = Parser<SpecTreeNodeParam[] | undefined>;

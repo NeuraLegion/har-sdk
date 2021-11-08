f1cc62b92587feba6f5e487996da7b2ad62b8441
Viachaslau Tyshkavets <pmstss@gmail.com>
2021-11-08 19:52:03 +0300

    fix(editor): pr requested changes
    - BaseTreeParser: suggested assertion method for stringify()

--- packages/editor/src/editor/BaseTreeParser.ts
+++ packages/editor/src/editor/BaseTreeParser.ts
@@ -3,15 +3,23 @@ import { TreeParser } from './TreeParser';
 import { dump, load } from 'js-yaml';
 
 export abstract class BaseTreeParser<T> implements TreeParser {
-  protected doc: T;
-  protected tree: SpecTreeNode;
-  protected format: 'yaml' | 'json';
+  protected doc: T | undefined;
+  protected tree: SpecTreeNode | undefined;
+  protected format: 'yaml' | 'json' | undefined;
 
   public abstract setup(source: string): Promise<void>;
   public abstract parse(): SpecTreeNode;
 
   public stringify(): string {
+    this.validateParsedSource();
+
     return this.format === 'yaml' ? dump(this.doc) : JSON.stringify(this.doc);
+  }
+
+  protected validateParsedSource(): void {
+    if (!this.doc) {
+      throw new Error('You have to call "parse" to initialize the document.');
+    }
   }
 
   protected async load(source: string, errorMessage: string): Promise<void> {

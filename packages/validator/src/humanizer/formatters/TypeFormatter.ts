ca9f9430f00778df04e5a8d596027d9854880dcb
Viachaslau Tyshkavets <pmstss@gmail.com>
2021-11-29 20:56:42 +0300

    fix(validator): wording for additionalProperty violation in multiple properties case
    fixes #22

--- packages/validator/src/humanizer/formatters/TypeFormatter.ts
+++ packages/validator/src/humanizer/formatters/TypeFormatter.ts
@@ -1,4 +1,5 @@
 import { Formatter } from './Formatter';
+import { WordingHelper } from './WordingHelper';
 import { ErrorObject } from 'ajv';
 
 export class TypeFormatter implements Formatter {
@@ -12,13 +13,13 @@ export class TypeFormatter implements Formatter {
       case 'enum': {
         const list = error.params.allowedValues.map((x) => JSON.stringify(x));
 
-        return `must be one of: ${this.humanizeList(list, 'or')}`;
+        return `must be one of: ${WordingHelper.humanizeList(list, 'or')}`;
       }
 
       case 'type': {
         const type = error.params.type;
         const list = Array.isArray(type) ? type : type.split(',');
-        const expectType = this.humanizeList(list, 'or');
+        const expectType = WordingHelper.humanizeList(list, 'or');
 
         return `must be of type ${expectType}`;
       }
@@ -27,28 +28,12 @@ export class TypeFormatter implements Formatter {
         const allowedValue = error.params.allowedValue;
 
         return Array.isArray(allowedValue)
-          ? `must be one of: ${this.humanizeList(
+          ? `must be one of: ${WordingHelper.humanizeList(
               allowedValue.map((x) => JSON.stringify(x)),
               'or'
             )}`
           : `must be equal to constant "${allowedValue}"`;
       }
     }
-  }
-
-  private humanizeList(arr: string[], conjunction = 'and'): string {
-    if (arr.length === 0) {
-      return 'nothing';
-    }
-    if (arr.length === 1) {
-      return arr[0];
-    }
-    if (arr.length === 2) {
-      return `${arr[0]} ${conjunction} ${arr[1]}`;
-    }
-
-    return `${arr.slice(0, -1).join(', ')}, ${conjunction} ${
-      arr[arr.length - 1]
-    }`;
   }
 }

00a2311626184cfff752b78b1415b0eee8ebedb0
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2021-12-06 10:06:17 +0300

    fix(validator): improve `serverIPAddress` schema
    closes #60

--- packages/validator/tests/har.spec.ts
+++ packages/validator/tests/har.spec.ts
@@ -34,16 +34,44 @@ describe('HarValidator', () => {
       result.should.be.empty;
     });
 
-    it.skip('should return error if server IP is not valid IP address', async () => {
+    it('should return error if server IP is not valid IP address', async () => {
       // arrange
       const input = invalidServerIP as unknown as Har;
       const expected: ErrorObject[] = [
         {
           instancePath: '/log/entries/0/serverIPAddress',
-          schemaPath: '#/properties/serverIPAddress/else/else/format',
-          keyword: 'format',
-          params: { format: 'ipv4' },
-          message: 'must match format "ipv4"'
+          schemaPath: '#/properties/serverIPAddress/errorMessage',
+          keyword: 'errorMessage',
+          params: {
+            errors: [
+              {
+                emUsed: true,
+                instancePath: '/log/entries/0/serverIPAddress',
+                schemaPath: '#/properties/serverIPAddress/else/then/format',
+                keyword: 'format',
+                params: { format: 'ipv4' },
+                message: 'must match format "ipv4"'
+              },
+              {
+                emUsed: true,
+                instancePath: '/log/entries/0/serverIPAddress',
+                schemaPath: '#/properties/serverIPAddress/else/if',
+                keyword: 'if',
+                params: { failingKeyword: 'then' },
+                message: 'must match "then" schema'
+              },
+              {
+                emUsed: true,
+                instancePath: '/log/entries/0/serverIPAddress',
+                schemaPath: '#/properties/serverIPAddress/if',
+                keyword: 'if',
+                params: { failingKeyword: 'else' },
+                message: 'must match "else" schema'
+              }
+            ]
+          },
+          message:
+            'The property `serverIPAddress` must have a value that is either IPv4 or IPv6 format'
         }
       ];
 

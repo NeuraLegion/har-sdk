fabbe489fe0f80a97d19dfd795319b0b2c9aafaa
Viachaslau Tyshkavets <pmstss@gmail.com>
2022-01-06 18:52:34 +0300

    test(openapi-sampler): specs for string format

--- packages/openapi-sampler/tests/primitives.spec.ts
+++ packages/openapi-sampler/tests/primitives.spec.ts
@@ -2,35 +2,42 @@ import { sample } from '../src';
 import 'chai/register-should';
 
 describe('Primitives', () => {
-  it('should sample string', () => {
+  it('should sample deterministic string', () => {
     const schema = {
       type: 'string'
     };
     const result = sample(schema);
-    result.should.be.a('string');
+    result.should.eq('lorem');
   });
+
   it('should sample number', () => {
     const schema = {
-      type: 'number'
+      type: 'number',
+      min: 10
     };
     const result = sample(schema);
+
+    // TODO
     result.should.be.a('number');
   });
-  it('should sample boolean', () => {
+
+  it('should sample deterministic boolean', () => {
     const schema = {
       type: 'boolean'
     };
     const result = sample(schema);
-    result.should.be.a('boolean');
+    result.should.eq(true);
   });
+
   it('should use default property', () => {
     const schema = {
       type: 'number',
       default: 100
     };
     const result = sample(schema);
-    result.should.equal(100);
+    result.should.eq(100);
   });
+
   it('should use null if type is not specified', () => {
     const schema = {};
     const result = sample(schema);

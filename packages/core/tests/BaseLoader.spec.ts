c0dd2f589c11c72e491477f3f3d1277d05392a98
Viachaslau Tyshkavets <pmstss@gmail.com>
2022-01-21 19:08:21 +0300

    test(core): test cases for base loader default behavior

---
+++ packages/core/tests/BaseLoader.spec.ts
@@ -0,0 +1,82 @@
+// eslint-disable-next-line max-classes-per-file
+import 'chai/register-should';
+import { BaseLoader } from '../src/loaders/BaseLoader';
+import { BaseSyntaxErrorDetailsExtractor } from '../src/loaders/errors/BaseSyntaxErrorDetailsExtractor';
+
+describe('BaseLoader', () => {
+  class CustomError extends Error {
+    constructor() {
+      super('CustomError message');
+    }
+  }
+
+  class CustomSyntaxErrorDetailsExtractor extends BaseSyntaxErrorDetailsExtractor<SyntaxError> {
+    protected extractOffset(): undefined {
+      return undefined;
+    }
+  }
+
+  class UnsupportedErrorLoader extends BaseLoader {
+    constructor() {
+      super(new CustomSyntaxErrorDetailsExtractor());
+    }
+
+    protected isSupportedError(): boolean {
+      return false;
+    }
+
+    protected parse(): unknown {
+      throw new CustomError();
+    }
+  }
+
+  class SupportedErrorLoader extends BaseLoader {
+    constructor() {
+      super(new CustomSyntaxErrorDetailsExtractor());
+    }
+
+    protected isSupportedError(): boolean {
+      return true;
+    }
+
+    protected parse(): unknown {
+      throw new CustomError();
+    }
+  }
+
+  it(`should return origin error message for unknown errors`, () => {
+    // arrange
+    const loader = new UnsupportedErrorLoader();
+
+    // act
+    try {
+      loader.load(null);
+    } catch {
+      // noop
+    }
+    const result = loader.getSyntaxErrorDetails();
+
+    // assert
+    result.should.deep.eq({
+      message: 'CustomError message'
+    });
+  });
+
+  it(`should return origin error message for known errors by default`, () => {
+    // arrange
+    const loader = new SupportedErrorLoader();
+
+    // act
+    try {
+      loader.load(null);
+    } catch {
+      // noop
+    }
+    const result = loader.getSyntaxErrorDetails();
+
+    // assert
+    result.should.deep.eq({
+      message: 'CustomError message'
+    });
+  });
+});

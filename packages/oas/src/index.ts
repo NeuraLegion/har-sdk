8a1e33f81f5ba42e78ba65ea6488b93180c03d97
Viachaslau Tyshkavets <pmstss@gmail.com>
2022-01-27 15:29:49 +0300

    fix(oas): export converter error classes
    fixes #103

--- packages/oas/src/index.ts
+++ packages/oas/src/index.ts
@@ -1,6 +1,8 @@
 import { DefaultConverter } from './converter';
 import { OpenAPI, Request } from '@har-sdk/core';
 
+export * from './errors';
+
 export const oas2har = (collection: OpenAPI.Document): Promise<Request[]> => {
   if (!collection) {
     throw new TypeError('Please provide a valid OAS specification.');

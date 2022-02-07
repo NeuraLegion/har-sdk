aab40f2cae3099c7a88ba623c6a2facde59d54f0
Viachaslau Tyshkavets <pmstss@gmail.com>
2022-02-07 10:39:29 +0300

    test(openapi-sampler): rework string spec

--- packages/openapi-sampler/tests/string.spec.ts
+++ packages/openapi-sampler/tests/string.spec.ts
@@ -2,127 +2,117 @@ import { sample } from '../src';
 import 'chai/register-should';
 
 describe('StringSampler', () => {
-  it('should sample deterministic email string', () => {
-    const schema = {
-      type: 'string',
-      format: 'email'
-    };
-    const result = sample(schema);
-    result.should.eq('jon.snow@targaryen.com');
+  [
+    {
+      input: {
+        type: 'string',
+        format: 'email'
+      },
+      expected: 'jon.snow@targaryen.com'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'pattern',
+        pattern: '\\d{1,3}'
+      },
+      expected: '44'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'password',
+        minLength: 25
+      },
+      expected: 'p@$$w0rdp@$$w0rdp@$$w0rdp'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'password',
+        maxLength: 4
+      },
+      expected: 'p@$$'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'binary'
+      },
+      expected: '\\x01\\x02\\x03\\x04\\x05'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'dummy'
+      },
+      expected: 'lorem'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'date-time'
+      },
+      expected: '2021-12-31T23:34:00Z'
+    }
+  ].forEach(({ input, expected }) => {
+    const { type, format, ...restrictions } = input;
+    const suffixStr = Object.keys(restrictions).length
+      ? ` with restrictions ${JSON.stringify(restrictions)}`
+      : '';
+    it(`should sample \`${format}\` format string${suffixStr}`, () => {
+      const result = sample(input);
+
+      result.should.eq(expected);
+    });
   });
 
-  it('should sample deterministic pattern string', () => {
-    const schema = {
-      type: 'string',
-      format: 'pattern',
-      pattern: '\\d{1,3}'
-    };
-    const result = sample(schema);
-    result.should.eq('44');
-  });
+  [
+    {
+      input: {
+        type: 'string',
+        maxLength: 5,
+        minLength: 10
+      },
+      expected:
+        'Cannot sample string by boundaries: minLength=10, maxLength=5, format=default'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'pattern',
+        pattern: '\\d{10,20}',
+        maxLength: 5
+      },
+      expected:
+        'Cannot sample string by boundaries: maxLength=5, format=pattern'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'date',
+        minLength: 11
+      },
+      expected: 'Cannot sample string by boundaries: minLength=11, format=date'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'date',
+        minLength: 11,
+        maxLength: 20
+      },
+      expected:
+        'Cannot sample string by boundaries: minLength=11, maxLength=20, format=date'
+    }
+  ].forEach(({ input, expected }) => {
+    const { type, ...restrictions } = input;
+    it(`should throw an error on controversial constrains ${JSON.stringify(
+      restrictions
+    )}`, () => {
+      const result = () => sample(input);
 
-  it('should throw on controversial minLength and maxLength constrains', () => {
-    const schema = {
-      type: 'string',
-      maxLength: 5,
-      minLength: 10
-    };
-
-    const result = () => sample(schema);
-
-    result.should.throw(
-      /Cannot sample string by boundaries: minLength=10, maxLength=5, format=default/
-    );
-  });
-
-  it('should throw on controversial maxLength and pattern constrains', () => {
-    const schema = {
-      type: 'string',
-      format: 'pattern',
-      pattern: '\\d{10,20}',
-      maxLength: 5
-    };
-
-    const result = () => sample(schema);
-
-    result.should.throw(
-      /Cannot sample string by boundaries: maxLength=5, format=pattern/
-    );
-  });
-
-  it('should sample deterministic long password', () => {
-    const schema = {
-      type: 'string',
-      format: 'password',
-      minLength: 25
-    };
-    const result = sample(schema);
-    result.should.eq('p@$$w0rdp@$$w0rdp@$$w0rdp');
-  });
-
-  it('should sample deterministic short password', () => {
-    const schema = {
-      type: 'string',
-      format: 'password',
-      maxLength: 4
-    };
-    const result = sample(schema);
-    result.should.eq('p@$$');
-  });
-
-  it('should sample deterministic binary string', () => {
-    const schema = {
-      type: 'string',
-      format: 'binary'
-    };
-    const result = sample(schema);
-    result.should.eq('\\x01\\x02\\x03\\x04\\x05');
-  });
-
-  it('should sample deterministic default value for unknown format', () => {
-    const schema = {
-      type: 'string',
-      format: 'dummy'
-    };
-    const result = sample(schema);
-    result.should.eq('lorem');
-  });
-
-  it('should sample deterministic date-time string', () => {
-    const schema = {
-      type: 'string',
-      format: 'date-time'
-    };
-    const result = sample(schema);
-    result.should.eq('2021-12-31T23:34:00Z');
-  });
-
-  it('should throw on invalid date minLength constrain', () => {
-    const schema = {
-      type: 'string',
-      format: 'date',
-      minLength: 11
-    };
-
-    const result = () => sample(schema);
-
-    result.should.throw(
-      /Cannot sample string by boundaries: minLength=11, format=date/
-    );
-  });
-
-  it('should throw on invalid date minLength and maxLength constrains', () => {
-    const schema = {
-      type: 'string',
-      format: 'date',
-      minLength: 11,
-      maxLength: 20
-    };
-
-    const result = () => sample(schema);
-
-    result.should.throw(
-      /Cannot sample string by boundaries: minLength=11, maxLength=20, format=date/
-    );
+      result.should.throw(expected);
+    });
   });
 });

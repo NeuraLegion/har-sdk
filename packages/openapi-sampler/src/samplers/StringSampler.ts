91d9a3979992e345b212866d3c3f0a100bd0835e
Viachaslau <pmstss@gmail.com>
2022-02-07 11:19:16 +0300

    fix(openapi-sampler): errors on string constrain violations (#123)
    fixes #122

--- packages/openapi-sampler/src/samplers/StringSampler.ts
+++ packages/openapi-sampler/src/samplers/StringSampler.ts
@@ -2,73 +2,80 @@ import { Sampler, OpenAPISchema } from './Sampler';
 import RandExp from 'randexp';
 
 export class StringSampler implements Sampler {
+  private readonly stringFormats = {
+    'email': () => 'jon.snow@targaryen.com',
+    'password': (min: number, max: number) =>
+      this.adjustLength('p@$$w0rd', min, max),
+    'date-time': () => '2021-12-31T23:34:00Z',
+    'date': () => '2021-12-31',
+    'ipv4': () => '208.67.222.222',
+    'ipv6': () => '0000:0000:0000:0000:0000:ffff:d043:dcdc',
+    'hostname': () => 'brokencrystals.com',
+    'uri': () => 'https://github.com/NeuraLegion/brokencrystals',
+    'byte': () => 'ZHVtbXkgYmluYXJ5IHNhbXBsZQA=',
+    'binary': () => '\\x01\\x02\\x03\\x04\\x05',
+    'base64': () => 'ZHVtbXkgYmluYXJ5IHNhbXBsZQA=',
+    'uuid': () => 'fbdf5a53-161e-4460-98ad-0e39408d8689',
+    'pattern': (
+      _min: number,
+      _max: number,
+      { pattern }: { pattern: string | RegExp }
+    ) => this.patternSample(pattern),
+    'default': (min: number, max: number) =>
+      this.adjustLength('lorem', min, max)
+  };
+
   public sample(schema: OpenAPISchema): any {
     const format = schema.pattern ? 'pattern' : schema.format || 'default';
-    const sampler = stringFormats[format] || defaultSample;
+    const sampler = this.stringFormats[format] || this.stringFormats['default'];
 
-    return sampler(schema.minLength || 0, schema.maxLength, schema.pattern);
+    const { minLength: min, maxLength: max } = schema;
+
+    return this.checkLength(sampler(min || 0, max, schema), format, min, max);
+  }
+
+  private patternSample(pattern: string | RegExp): string {
+    const randExp = new RandExp(pattern);
+    randExp.randInt = (a, b) => Math.floor((a + b) / 2);
+
+    return randExp.gen();
+  }
+
+  private checkLength(
+    value: string,
+    format: string,
+    min: number,
+    max: number
+  ): string {
+    if ((min && value.length < min) || (max && value.length > max)) {
+      const pairs = [
+        { key: 'minLength', value: min },
+        { key: 'maxLength', value: max },
+        { key: 'format', value: format }
+      ];
+
+      const boundariesStr = pairs
+        .filter((p) => p.value !== undefined)
+        .map((p) => `${p.key}=${p.value}`)
+        .join(', ');
+
+      throw new Error(`Cannot sample string by boundaries: ${boundariesStr}`);
+    }
+
+    return value;
+  }
+
+  private adjustLength(sample: string, min: number, max: number): string {
+    const minLength = min ? min : 0;
+    const maxLength = max ? max : sample.length;
+
+    return minLength > sample.length
+      ? sample
+          .repeat(Math.trunc(minLength / sample.length) + 1)
+          .substring(0, minLength)
+      : sample.substr(
+          0,
+          Math.min(Math.max(sample.length, minLength), maxLength)
+        );
   }
 }
-
-const checkLength = (
-  value: string,
-  format: string,
-  min: number,
-  max: number
-) => {
-  if (min && value.length < min) {
-    throw new Error(
-      `Using minLength = ${min} is incorrect with format "${format}"`
-    );
-  }
-
-  if (max && value.length > max) {
-    throw new Error(
-      `Using maxLength = ${max} is incorrect with format "${format}"`
-    );
-  }
-
-  return value;
-};
-
-const adjustLength = (sample: string, min: number, max: number): string => {
-  const minLength = min ? min : 0;
-  const maxLength = max ? max : sample.length;
-
-  return minLength > sample.length
-    ? sample
-        .repeat(Math.trunc(minLength / sample.length) + 1)
-        .substring(0, minLength)
-    : sample.substr(0, Math.min(Math.max(sample.length, minLength), maxLength));
-};
-
-const defaultSample = (min: number, max: number) =>
-  adjustLength('lorem', min, max);
-
-const patternSample = (min: number, max: number, pattern: string | RegExp) => {
-  const randExp = new RandExp(pattern);
-  randExp.randInt = (a, b) => Math.floor((a + b) / 2);
-
-  return checkLength(randExp.gen(), 'pattern', min, max);
-};
-
-const base64sample = () => 'ZHVtbXkgYmluYXJ5IHNhbXBsZQA=';
-
-const stringFormats = {
-  'email': () => 'jon.snow@targaryen.com',
-  'password': (min: number, max: number) => adjustLength('p@$$w0rd', min, max),
-  'date-time': (min: number, max: number) =>
-    checkLength('2021-12-31T23:34:00Z', 'date-time', min, max),
-  'date': (min: number, max: number) =>
-    checkLength('2021-12-31', 'date', min, max),
-  'ipv4': () => '208.67.222.222',
-  'ipv6': () => '0000:0000:0000:0000:0000:ffff:d043:dcdc',
-  'hostname': () => 'brokencrystals.com',
-  'uri': () => 'https://github.com/NeuraLegion/brokencrystals',
-  'byte': base64sample,
-  'binary': () => '\\x01\\x02\\x03\\x04\\x05',
-  'base64': base64sample,
-  'uuid': () => 'fbdf5a53-161e-4460-98ad-0e39408d8689',
-  'pattern': patternSample,
-  'default': defaultSample
-};
--- packages/openapi-sampler/tests/string.spec.ts
+++ packages/openapi-sampler/tests/string.spec.ts
@@ -2,96 +2,182 @@ import { sample } from '../src';
 import 'chai/register-should';
 
 describe('StringSampler', () => {
-  it('should sample deterministic email string', () => {
-    const schema = {
-      type: 'string',
-      format: 'email'
-    };
-    const result = sample(schema);
-    result.should.eq('jon.snow@targaryen.com');
+  [
+    {
+      input: {
+        type: 'string',
+        format: 'email'
+      },
+      expected: 'jon.snow@targaryen.com'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'pattern',
+        pattern: '\\d{1,3}'
+      },
+      expected: '44'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'password',
+        minLength: 25
+      },
+      expected: 'p@$$w0rdp@$$w0rdp@$$w0rdp'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'password',
+        maxLength: 4
+      },
+      expected: 'p@$$'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'binary'
+      },
+      expected: '\\x01\\x02\\x03\\x04\\x05'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'dummy'
+      },
+      expected: 'lorem'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'date-time'
+      },
+      expected: '2021-12-31T23:34:00Z'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'ipv4',
+        maxLength: 100
+      },
+      expected: '208.67.222.222'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'ipv6',
+        minLength: 39
+      },
+      expected: '0000:0000:0000:0000:0000:ffff:d043:dcdc'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'hostname',
+        minLength: 1
+      },
+      expected: 'brokencrystals.com'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'uri'
+      },
+      expected: 'https://github.com/NeuraLegion/brokencrystals'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'byte',
+        minLength: 1,
+        maxLength: 100
+      },
+      expected: 'ZHVtbXkgYmluYXJ5IHNhbXBsZQA='
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'base64',
+        minLength: 28
+      },
+      expected: 'ZHVtbXkgYmluYXJ5IHNhbXBsZQA='
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'uuid',
+        minLength: 1,
+        maxLength: 36
+      },
+      expected: 'fbdf5a53-161e-4460-98ad-0e39408d8689'
+    }
+  ].forEach(({ input, expected }) => {
+    const { type, format, ...restrictions } = input;
+    const suffixStr = Object.keys(restrictions).length
+      ? ` with restrictions ${JSON.stringify(restrictions)}`
+      : '';
+    it(`should sample \`${format}\` format string${suffixStr}`, () => {
+      const result = sample(input);
+
+      result.should.eq(expected);
+    });
   });
 
-  it('should sample deterministic pattern string', () => {
-    const schema = {
-      type: 'string',
-      format: 'pattern',
-      pattern: '\\d{1,3}'
-    };
-    const result = sample(schema);
-    result.should.eq('44');
-  });
+  [
+    {
+      input: {
+        type: 'string',
+        maxLength: 5,
+        minLength: 10
+      },
+      expected:
+        'Cannot sample string by boundaries: minLength=10, maxLength=5, format=default'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'pattern',
+        pattern: '\\d{10,20}',
+        maxLength: 5
+      },
+      expected:
+        'Cannot sample string by boundaries: maxLength=5, format=pattern'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'date',
+        minLength: 11
+      },
+      expected: 'Cannot sample string by boundaries: minLength=11, format=date'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'date',
+        minLength: 1,
+        maxLength: 9
+      },
+      expected:
+        'Cannot sample string by boundaries: minLength=1, maxLength=9, format=date'
+    },
+    {
+      input: {
+        type: 'string',
+        format: 'uuid',
+        maxLength: 35
+      },
+      expected: 'Cannot sample string by boundaries: maxLength=35, format=uuid'
+    }
+  ].forEach(({ input, expected }) => {
+    const { type, ...restrictions } = input;
+    it(`should throw an error on controversial constrains ${JSON.stringify(
+      restrictions
+    )}`, () => {
+      const result = () => sample(input);
 
-  it('should throw on invalid maxLength pattern constrain', () => {
-    const schema = {
-      type: 'string',
-      format: 'pattern',
-      pattern: '\\d{10,20}',
-      maxLength: 5
-    };
-
-    const result = () => sample(schema);
-
-    result.should.throw(
-      /Using maxLength = 5 is incorrect with format "pattern"/
-    );
-  });
-
-  it('should sample deterministic long password', () => {
-    const schema = {
-      type: 'string',
-      format: 'password',
-      minLength: 25
-    };
-    const result = sample(schema);
-    result.should.eq('p@$$w0rdp@$$w0rdp@$$w0rdp');
-  });
-
-  it('should sample deterministic short password', () => {
-    const schema = {
-      type: 'string',
-      format: 'password',
-      maxLength: 4
-    };
-    const result = sample(schema);
-    result.should.eq('p@$$');
-  });
-
-  it('should sample deterministic binary string', () => {
-    const schema = {
-      type: 'string',
-      format: 'binary'
-    };
-    const result = sample(schema);
-    result.should.eq('\\x01\\x02\\x03\\x04\\x05');
-  });
-
-  it('should sample deterministic default value for unknown format', () => {
-    const schema = {
-      type: 'string',
-      format: 'dummy'
-    };
-    const result = sample(schema);
-    result.should.eq('lorem');
-  });
-
-  it('should sample deterministic date-time string', () => {
-    const schema = {
-      type: 'string',
-      format: 'date-time'
-    };
-    const result = sample(schema);
-    result.should.eq('2021-12-31T23:34:00Z');
-  });
-
-  it('should throw on invalid date minLength constrain', () => {
-    const schema = {
-      type: 'string',
-      format: 'date',
-      minLength: 11
-    };
-
-    const result = () => sample(schema);
-
-    result.should.throw(/Using minLength = 11 is incorrect with format "date"/);
+      result.should.throw(expected);
+    });
   });
 });

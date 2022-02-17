244b01629036740b779ef26abd47130031b7168a
Viachaslau Tyshkavets <pmstss@gmail.com>
2022-02-17 13:26:08 +0300

    chore: resolved todo

--- packages/oas/src/utils/index.ts
+++ packages/oas/src/utils/index.ts
@@ -4,7 +4,6 @@ export * from './Flattener';
 export * from './isObject';
 export * from './Params';
 
-// TODO extract to core?
 export const isOASV2 = (doc: OpenAPI.Document): doc is OpenAPIV2.Document =>
   'swagger' in doc;
 export const isOASV3 = (doc: OpenAPI.Document): doc is OpenAPIV3.Document =>

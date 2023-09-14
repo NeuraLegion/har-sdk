749b6d8dd0de2e431a9becb2ed0a590dd520426a
ostridm <dmitry.ostrikov@brightsec.com>
2023-09-14 17:47:48 +0300

    fix(sampler): introduce max-length support for string pattern format sample (#209)
    closes #208

--- packages/openapi-sampler/src/samplers/StringSampler.ts
+++ packages/openapi-sampler/src/samplers/StringSampler.ts
@@ -31,9 +31,9 @@ export class StringSampler implements Sampler {
     'regex': () => '/regex/',
     'pattern': (
       _min: number,
-      _max: number,
+      max: number,
       { pattern }: { pattern: string | RegExp }
-    ) => this.patternSample(pattern),
+    ) => this.patternSample(pattern, max),
     'default': (min: number, max: number) =>
       this.adjustLength('lorem', min, max)
   };
@@ -47,8 +47,10 @@ export class StringSampler implements Sampler {
     return this.checkLength(sampler(min || 0, max, schema), format, min, max);
   }
 
-  private patternSample(pattern: string | RegExp): string {
+  private patternSample(pattern: string | RegExp, max?: number): string {
     const randExp = new RandExp(pattern);
+
+    randExp.max = max ?? randExp.max;
     randExp.randInt = (a, b) => Math.floor((a + b) / 2);
 
     return randExp.gen();

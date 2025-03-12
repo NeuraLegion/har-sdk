6f8fa520d60e798ae465593ec78a015592244ddc
dostrikov <dmitry.ostrikov@brightsec.com>
2025-03-12 16:33:15 +0400

    build(deps-dev): bump jest from 28.1.0 to 29.7.0 (#265)

--- packages/editor/jest.config.js
+++ packages/editor/jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   displayName: 'editor',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json'
-    }
-  },
   transform: {
     '^.+\\.[tj]s$': 'ts-jest'
   },
